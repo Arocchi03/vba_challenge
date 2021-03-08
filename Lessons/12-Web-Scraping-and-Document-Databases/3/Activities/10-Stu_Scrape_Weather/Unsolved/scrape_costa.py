7a302178968809b125f1347915fc0892250df90c
Teacher Travis <travis@travisrtaylor.com>
2021-03-08 17:26:37 -0600

    Added 12.3 activities

---
+++ Lessons/12-Web-Scraping-and-Document-Databases/3/Activities/10-Stu_Scrape_Weather/Unsolved/scrape_costa.py
@@ -0,0 +1,48 @@
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+
+def init_browser():
+    # Setup splinter
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    return Browser("chrome", **executable_path, headless=False)
+
+
+def scrape_info():
+    browser = init_browser()
+
+    # Visit visitcostarica.herokuapp.com
+    url = "https://visitcostarica.herokuapp.com/"
+    browser.visit(url)
+
+    time.sleep(1)
+
+    # Scrape page into Soup
+    html = browser.html
+    soup = bs(html, "html.parser")
+
+    # Get the average temps
+    # @TODO: YOUR CODE HERE!
+
+    # Get the min avg temp
+    # @TODO: YOUR CODE HERE!
+
+    # Get the max avg temp
+    # @TODO: YOUR CODE HERE!
+
+    # BONUS: Find the src for the sloth image
+    # @TODO: YOUR CODE HERE!
+
+    # Store data in a dictionary
+    costa_data = {
+        "sloth_img": sloth_img,
+        "min_temp": min_temp,
+        "max_temp": max_temp
+    }
+
+    # Quite the browser after scraping
+    browser.quit()
+
+    # Return results
+    return costa_data

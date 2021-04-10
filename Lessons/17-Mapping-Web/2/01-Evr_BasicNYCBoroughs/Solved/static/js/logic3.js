9f8471099521054ec96a4d973b463c7851a39c0f
Jay <59891009+jayyufreeosk@users.noreply.github.com>
2021-04-10 10:17:09 -0500

    17.2 Evr Solutions

---
+++ Lessons/17-Mapping-Web/2/01-Evr_BasicNYCBoroughs/Solved/static/js/logic3.js
@@ -0,0 +1,46 @@
+// Creating the map object
+var myMap = L.map("map", {
+  center: [40.7128, -74.0059],
+  zoom: 11
+});
+
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+// Use this link to get the GeoJSON data.
+var link = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/15-Mapping-Web/nyc.geojson";
+
+// The function that will determine the color of a neighborhood based on the borough that it belongs to
+function chooseColor(borough) {
+  switch (borough) {
+  case "Brooklyn":
+    return "yellow";
+  case "Bronx":
+    return "red";
+  case "Manhattan":
+    return "orange";
+  case "Queens":
+    return "green";
+  case "Staten Island":
+    return "purple";
+  default:
+    return "black";
+  }
+}
+
+// Getting our GeoJSON data
+d3.json(link).then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data
+  L.geoJson(data, {
+    style: function(feature) {
+      return {
+        color: "white",
+        fillColor: chooseColor(feature.properties.borough),
+        fillOpacity: 0.5,
+        weight: 1.5
+      };
+    }
+  }).addTo(myMap);
+});
